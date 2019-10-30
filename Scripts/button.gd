3009c594908095fd2f4ed0e4774261957dab0594
seoLegna <49617795+seoLegna@users.noreply.github.com>
2019-10-30 10:22:17 -0400

    done

---
+++ Scripts/button.gd
@@ -0,0 +1,4 @@
+extends Button
+
+func _toggled(button_pressed):
+	get_tree().change_scene("res://Scenes/Game.tscn")
---
+++ Scripts/end_score.gd
@@ -0,0 +1,7 @@
+extends Label
+
+func _ready():
+	update_score()
+
+func update_score():
+	text = finde
